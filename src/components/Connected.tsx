f31d4fe783a8bd9512c81710b998685e8e85608c
noseagenft <noseagenft@gmail.com>
2023-02-24 00:26:49 +0800

    Init

---
+++ src/components/Connected.tsx
@@ -0,0 +1,54 @@
+import { FC } from "react";
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+  Image,
+} from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+
+const Connected: FC = () => {
+  return (
+    <VStack spacing={20}>
+      <Container>
+        <VStack spacing={8}>
+          <Heading
+            color="white"
+            as="h1"
+            size="2xl"
+            noOfLines={1}
+            textAlign="center"
+          >
+            Welcome Buildoor.
+          </Heading>
+
+          <Text color="bodyText" fontSize="xl" textAlign="center">
+            Each buildoor is randomly generated and can be staked to receive
+            <Text as="b"> $BLD</Text> Use your <Text as="b"> $BLD</Text> to
+            upgrade your buildoor and receive perks within the community!
+          </Text>
+        </VStack>
+      </Container>
+
+      <HStack spacing={10}>
+        <Image src="canyouhearme.png" alt="" w={150} />
+        <Image src="hypnotise.png" alt="" w={150} />
+        <Image src="icanseemybrain.png" alt="" w={150} />
+        <Image src="myeyesareopenla.png" alt="" w={150} />
+        <Image src="pkbestmod.png" alt="" w={150} />
+      </HStack>
+
+      <Button bgColor="accent" color="white" maxW="380px">
+        <HStack>
+          <Text>mint buildoor</Text>
+          <ArrowForwardIcon />
+        </HStack>
+      </Button>
+    </VStack>
+  );
+};
+
+export default Connected;
